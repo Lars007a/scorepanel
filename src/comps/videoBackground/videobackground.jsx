ff090496e58c5bebf6f441f98d9e8e1f5fee8252
Lars <laarrss@protonmail.com>
2025-04-21 01:59:35 +0200

    ting

--- src/comps/videoBackground/videobackground.jsx
+++ src/comps/videoBackground/videobackground.jsx
@@ -1,11 +1,12 @@
 import styles from "./videobackground.module.css";
+import video from "../../assets/bcvideo_cropped2.mp4";
 
 export default function videobc({ children }) {
   return (
     <>
       <section className={styles.bc}>
         <video autoPlay muted loop id="bcVideo">
-          <source src="/video/bcvideo_cropped2.mp4" type="video/mp4" />
+          <source src={video} type="video/mp4" />
         </video>
         {children}
       </section>
